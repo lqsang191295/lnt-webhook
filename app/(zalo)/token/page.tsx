963e3d907721a3e4f1e539ef45c5d12647129b35
sangledev95 <lqsang19952@gmail.com>
2025-03-12 13:59:14 +0700

    Update

--- app/(zalo)/token/page.tsx
+++ app/(zalo)/token/page.tsx
@@ -14,7 +14,7 @@ import { Copy, Loader } from "lucide-react";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
-export async function getTokenProps() {
+export async function getStaticProps() {
   const data = await get(`/database/token`);
 
   return {
@@ -113,7 +113,7 @@ const PageToken = () => {
     try {
       setLoadingToken(true);
 
-      const { props } = await getTokenProps();
+      const { props } = await getStaticProps();
 
       if (!props.data) return;
 
