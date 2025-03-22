3eb787d5b08c15296a45211d010461ac89b52422
sangledev95 <lqsang19952@gmail.com>
2025-03-22 13:24:27 +0700

    Update

---
+++ app/(zalo)/gui-zns/_components/gui-zns-journey.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export function GuiZnsJourney() {
+  return <div className="flex flex-col">gui-zns-journey</div>;
+}
--- app/(zalo)/gui-zns/page.tsx
+++ app/(zalo)/gui-zns/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { SidebarPanel } from "./_components/side-bar-panel";
+
 const PageGuiZNS = () => {
-  return <div className="p-4 w-full h-full">Gui ZNS</div>;
+  return (
+    <div className="p-4 w-full h-full">
+      <SidebarPanel />
+    </div>
+  );
 };
 
 export default PageGuiZNS;
