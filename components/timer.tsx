29b0c7a85f2366c458070fd1c3cabab4b0d16ada
sangledev95 <lqsang19952@gmail.com>
2025-03-24 17:30:29 +0700

    Update

--- components/timer.tsx
+++ components/timer.tsx
@@ -1,11 +1,8 @@
-import { TimerIcon } from "lucide-react";
-import React, {
-  useState,
-  useRef,
-  useEffect,
-  forwardRef,
-  useImperativeHandle,
-} from "react";
+"use client";
+
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import React, { useState, forwardRef, useImperativeHandle } from "react";
+import { motion } from "framer-motion";
 
 interface TimerProps {
   duration: number;
@@ -16,16 +13,44 @@ export interface TimerHandle {
   start: () => void;
 }
 
+const RenderTime = ({ remainingTime }: { remainingTime: number }) => {
+  if (remainingTime === 0) {
+    return <div className="timer">Hết giờ</div>;
+  }
+
+  return (
+    <div className="timer relative w-full h-full">
+      {remainingTime < 5 && (
+        <motion.div
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{
+            scale: [0, 0.88, 0.88],
+            opacity: [0, 0.5, 0],
+          }}
+          transition={{
+            duration: 1,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatDelay: 1,
+          }}
+          className="w-full h-full bg-red-500 rounded-full absolute z-0"
+        />
+      )}
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <div className="text">Remaining</div>
+        <div className="value">{remainingTime}</div>
+        <div className="text">seconds</div>
+      </div>
+    </div>
+  );
+};
+
 const Timer = forwardRef<TimerHandle, TimerProps>(
   ({ duration = 10, onTimeout }, ref) => {
-    const [timeLeft, setTimeLeft] = useState(duration);
-    const [isStarted, setIsStarted] = useState(false);
-    const timerRef = useRef<ReturnType<typeof setInterval> | null>(null);
+    const [isPlay, setIsPlay] = useState<boolean>(false);
 
     const start = () => {
-      if (timerRef.current) return;
-
-      setIsStarted(true);
+      setIsPlay(true);
     };
 
     // Expose start method using ref
@@ -33,39 +58,17 @@ const Timer = forwardRef<TimerHandle, TimerProps>(
       start,
     }));
 
-    useEffect(() => {
-      if (!isStarted) return;
-
-      if (timerRef.current) return;
-
-      timerRef.current = setInterval(() => {
-        console.log("run inter val", isStarted);
-
-        setTimeLeft((prev) => {
-          if (prev <= 1) {
-            clearInterval(timerRef.current!);
-            timerRef.current = null;
-            onTimeout?.();
-            return 0;
-          }
-          return prev - 1;
-        });
-      }, 1000);
-    }, [isStarted, onTimeout]);
-
-    useEffect(() => {
-      return () => {
-        if (timerRef.current) {
-          clearInterval(timerRef.current);
-          timerRef.current = null;
-        }
-      };
-    }, []);
-
     return (
-      <div className="flex">
-        <TimerIcon /> {timeLeft}s
-      </div>
+      <CountdownCircleTimer
+        isPlaying={isPlay}
+        duration={duration}
+        colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+        colorsTime={[10, 6, 3, 0]}
+        onComplete={onTimeout}
+        size={120}
+        strokeWidth={8}>
+        {RenderTime}
+      </CountdownCircleTimer>
     );
   }
 );
