55e0af74ac6debe4efd30743ac7960ccf230b3c5
lqsang191295 <lqsang191295>
2025-06-20 08:46:17 +0700

    Update

--- app/waiting-screen/_components/footer.tsx
+++ app/waiting-screen/_components/footer.tsx
@@ -2,7 +2,11 @@ import { memo } from "react"
 
 const Footer = () => {
     return <div className="bg-green-900 text-white text-center py-3 text-base font-extrabold rounded-b-md flex-shrink-0 tracking-wider select-none">
-        Hotline hỗ trợ tải &amp; sử dụng App: 0276 3836 991 - 0941 696 939
+        <div className="grid grid-cols-12">
+            <div className="col-span-4">Tổng đài: 02763 797999</div>
+            <div className="col-span-4">Hotline: 1900 561 510</div>
+            <div className="col-span-4">Cấp cứu: 0888 79 52 59</div>
+        </div>
     </div>
 }
 
