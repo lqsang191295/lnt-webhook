9579c1870c62138ec09b5568f57044581ef272e0
lqsang191295 <lqsang191295>
2025-06-26 10:05:22 +0700

    Update

--- app/(doctor&patient)/_components/doctors/content-quet-qr.tsx
+++ app/(doctor&patient)/_components/doctors/content-quet-qr.tsx
@@ -40,9 +40,9 @@ export default function QRZaloStyle() {
                     (decodedText) => {
                         alert(decodedText);
                     },
-                    // (errorMessage) => {
-                    //     alert(errorMessage);
-                    // }
+                    () => {
+                        //
+                    }
                 );
             });
         }
